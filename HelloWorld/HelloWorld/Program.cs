db65dfba138f672908a6b5c05fac4f024fca8e6e
Tung Pham <tungint@gmail.com>
2018-03-09 10:55:44 -0500

    first practice

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            if (name == "Santa")
+            {
+                Console.WriteLine("Cant believe it's Santa");
+            }
+            else
+            {
+                Console.WriteLine("Hello {0}, nice to meet you {0}!", name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
