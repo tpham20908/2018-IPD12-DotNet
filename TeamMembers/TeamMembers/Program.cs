1d63ea968c5e56659228f97ad3d8adef09b7cde2
unknown <tpham20908@gmail.com>
2018-03-10 18:40:01 -0500

    TeamMembers project

--- TeamMembers/TeamMembers/Program.cs
+++ TeamMembers/TeamMembers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,41 @@ namespace TeamMembers
     {
         static void Main(string[] args)
         {
+            Dictionary<string, List<string>> playerTeams = new Dictionary<string, List<string>>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(@"../../../input.txt");
+                foreach (string line in lines)
+                {
+                    string teamName = line.Split(':')[0];
+                    string[] playersName = line.Split(':')[1].Split(',');
+                    foreach (string playerName in playersName)
+                    {
+                        if (!playerTeams.ContainsKey(playerName))
+                        {
+                            playerTeams.Add(playerName, new List<string>());
+                        }
+                        playerTeams[playerName].Add(teamName);
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File Not Found Error: " + ex.Message);
+            }
+
+            foreach (KeyValuePair<string, List<string>> playerTeam in playerTeams)
+            {
+                string teamName = "";
+                foreach (string team in playerTeam.Value)
+                {
+                    teamName += team + ", ";
+                }
+                Console.WriteLine("{0} plays for {1}", playerTeam.Key, teamName);
+            }
+            
+            Console.ReadLine();
         }
     }
 }
