c20ec022ed60419d55a9dcb029928930c236123d
unknown <tpham20908@gmail.com>
2018-03-10 20:20:08 -0500

    TeamMembers project v2

--- TeamMembers/TeamMembers/Program.cs
+++ TeamMembers/TeamMembers/Program.cs
@@ -18,6 +18,41 @@ namespace TeamMembers
                 string[] lines = File.ReadAllLines(@"../../../input.txt");
                 foreach (string line in lines)
                 {
+                    string team = line.Split(':')[0];
+                    string[] players = line.Split(':')[1].Split(',');
+                    foreach (string player in players)
+                    {
+                        if (!playerTeams.ContainsKey(player))
+                        {
+                            playerTeams.Add(player, new List<string>());
+                        }
+                        playerTeams[player].Add(team);
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File not found error: " + ex.Message);
+            }
+
+            foreach (KeyValuePair<string, List<string>> playerTeam in playerTeams)
+            {
+                string player = playerTeam.Key;
+                string teams = "";
+                foreach (string team in playerTeam.Value)
+                {
+                    teams += team + ", ";
+                }
+                Console.WriteLine("{0} plays for {1}", player, teams);
+            }
+
+            /*
+            Dictionary<string, List<string>> playerTeams = new Dictionary<string, List<string>>();
+            try
+            {
+                string[] lines = File.ReadAllLines(@"../../../input.txt");
+                foreach (string line in lines)
+                {
                     string teamName = line.Split(':')[0];
                     string[] playersName = line.Split(':')[1].Split(',');
                     foreach (string playerName in playersName)
@@ -44,7 +79,7 @@ namespace TeamMembers
                 }
                 Console.WriteLine("{0} plays for {1}", playerTeam.Key, teamName);
             }
-            
+            */
             Console.ReadLine();
         }
     }
