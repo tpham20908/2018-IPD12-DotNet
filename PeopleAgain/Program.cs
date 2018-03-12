70e5afca8336394b3246d8900893c2265e5bb35d
unknown <tpham20908@gmail.com>
2018-03-12 14:57:50 -0400

    PeopleAgain: done with Class Teacher

--- PeopleAgain/Program.cs
+++ PeopleAgain/Program.cs
@@ -22,9 +22,14 @@ namespace PeopleAgain
                 String _gpaStr = Console.ReadLine();
                 Console.WriteLine("Enter your program: ");
                 String _program = Console.ReadLine();
+                Console.WriteLine("Enter your subject: ");
+                String _subject = Console.ReadLine();
+                Console.WriteLine("Enter your experience: ");
+                String _experienceStr = Console.ReadLine();
                 Person p;
                 Student s;
-                if (Int32.TryParse(_ageStr, out int _age))
+                Teacher t;
+                if (int.TryParse(_ageStr, out int _age))
                 {
                     p = new Person(_name, _age);
                 }
@@ -32,7 +37,7 @@ namespace PeopleAgain
                 {
                     throw new InvalidDataException("Wrong input for age");
                 }
-                if (Int32.TryParse(_gpaStr, out int _gpa))
+                if (Double.TryParse(_gpaStr, out Double _gpa))
                 {
                     s = new Student(_name, _age, _gpa, _program);
                 }
@@ -40,7 +45,17 @@ namespace PeopleAgain
                 {
                     throw new InvalidDataException("Wrong input for GPA");
                 }
+                if (int.TryParse(_experienceStr, out int _experience))
+                {
+                    t = new Teacher(_name, _age, _subject, _experience);
+                }
+                else
+                {
+                    throw new InvalidDataException("Wrong input for GPA");
+                }
                 Console.WriteLine(p.ToString());
+                Console.WriteLine(s.ToString());
+                Console.WriteLine(t.ToString());
             }
             catch (InvalidDataException ex)
             {
@@ -52,6 +67,55 @@ namespace PeopleAgain
             }
             
             Console.ReadLine();
+        }
+    }
+
+    class Teacher : Person
+    {
+        public Teacher (String _name, int _age, String _subject, int _experience) : base (_name, _age)
+        {
+            Subject = _subject;
+            Experience = _experience;
+        }
+
+        private String _subject;
+        public String Subject
+        {
+            get
+            {
+                return _subject;
+            }
+            set
+            {
+                if (value.Length == 0 ||value.Length > 20)
+                {
+                    throw new InvalidDataException("Subject must be in between 1 and 20 characters");
+                }
+                _subject = value;
+            }
+        }
+
+        private int _experience;
+        public int Experience
+        {
+            get
+            {
+                return _experience;                     
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new InvalidDataException("Experience must be in between 0 and 100.");
+                }
+                _experience = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Teacher " + base.ToString() 
+                + String.Format(", subject {0}, {1} year(s) experience", _subject, _experience);
         }
     }
 
@@ -89,7 +153,7 @@ namespace PeopleAgain
             }
             set
             {
-                if (value.Length > 20)
+                if (value.Length == 0 || value.Length > 20)
                 {
                     throw new InvalidDataException("Name of Program cannot exceed 20 characters long.");
                 }
@@ -99,7 +163,7 @@ namespace PeopleAgain
 
         public override string ToString()
         {
-            return base.ToString() + String.Format(", GPA {0}, program {1}", _gpa, _program);
+            return "Student " + base.ToString() + String.Format(", GPA {0}, program {1}", _gpa, _program);
         }
     }
 
