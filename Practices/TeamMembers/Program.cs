4ebbab76180b2ee845b55f808b2c1ebaade99ff8
unknown <tpham20908@gmail.com>
2018-03-11 22:18:05 -0400

    Practices with TeamMembers

--- Practices/TeamMembers/Program.cs
+++ Practices/TeamMembers/Program.cs
@@ -14,11 +14,26 @@ namespace TeamMembers
             try
             {
                 string[] lines = File.ReadAllLines(@"../../input.txt");
+                Dictionary<string, List<string>> playerTeams = new Dictionary<string, List<string>>();
                 foreach (string line in lines)
                 {
-                    Console.WriteLine(line);
+                    string team = line.Split(':')[0];
+                    string[] players = line.Split(':')[1].Split(',');
+                    foreach (string player in players)
+                    {
+                        if (!playerTeams.ContainsKey(player))
+                        {
+                            playerTeams.Add(player, new List<string>());
+                        }
+                        playerTeams[player].Add(team);
+                    }
                 }
-                
+                foreach (KeyValuePair<string, List<string>> playerTeam in playerTeams)
+                {
+                    string player = playerTeam.Key;
+                    string teams = String.Join(", ", playerTeam.Value);
+                    Console.WriteLine("{0} plays for {1}", player, teams);
+                }
             }
             catch (FileNotFoundException ex)
             {
