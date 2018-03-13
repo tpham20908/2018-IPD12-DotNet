7c1c7b1a1ebecf19049f41e753e01a679e12894c
Tung Pham <tungint@gmail.com>
2018-03-13 13:00:22 -0400

    Sorted list practice with SortEasy project using LINQ

--- SortEasy/Program.cs
+++ SortEasy/Program.cs
@@ -8,8 +8,8 @@ namespace SortEasy
 {
     class Person
     {
-        public String name;
-        public int age;
+        public String Name;
+        public int Age;
     }
 
     class Program
@@ -19,15 +19,25 @@ namespace SortEasy
             List<Person> list = new List<Person>();
 
             // Using "Object Intializer
-            list.Add(new Person() { name = "Jerry", age = 22 });
-            list.Add(new Person() { name = "Emily", age = 32 });
-            list.Add(new Person() { name = "Judy", age = 25 });
-            list.Add(new Person() { name = "Tommy", age = 29 });
-            list.Add(new Person() { name = "Frank", age = 43 });
+            list.Add(new Person() { Name = "Jerry", Age = 22 });
+            list.Add(new Person() { Name = "Emily", Age = 32 });
+            list.Add(new Person() { Name = "Judy", Age = 25 });
+            list.Add(new Person() { Name = "Tommy", Age = 29 });
+            list.Add(new Person() { Name = "Frank", Age = 43 });
+
+            // LINQ sorting
+            var listByName = from p in list orderby p.Name select p;
+            // convert to List if needed
+            List<Person> personListByName = listByName.ToList();
 
             foreach (Person p in list)
             {
-                Console.WriteLine("{0} is {1} y/o", p.name, p.age);
+                Console.WriteLine("{0} is {1} y/o", p.Name, p.Age);
+            }
+            Console.WriteLine("=== Order by Name ===");
+            foreach (Person p in listByName)
+            {
+                Console.WriteLine("{0} is {1} y/o", p.Name, p.Age);
             }
             Console.ReadLine();
         }
