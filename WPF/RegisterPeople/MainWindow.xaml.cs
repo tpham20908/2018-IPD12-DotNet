b5830d3ed83781c95fe18ddc7762355c55b3db75
unknown <tpham20908@gmail.com>
2018-03-15 14:53:56 -0400

    RegisterPeople practice with WPF

--- WPF/RegisterPeople/MainWindow.xaml.cs
+++ WPF/RegisterPeople/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,5 +26,40 @@ namespace RegisterPeople
             InitializeComponent();
         }
 
+        private void btnRegister_Click_1(object sender, RoutedEventArgs e)
+        {
+            String name;
+            Object gender = null;
+            String pets = "";
+            int age = 0;
+            if (tbName.Text.Equals(""))
+            {
+                MessageBox.Show("Name cannot be empty", "Wrong name input");
+            }
+            name = tbName.Text;
+            
+            if (!int.TryParse(tbAge.Text, out int ageInput))
+            {
+                MessageBox.Show("Age must be an integer", "Wrong age input");
+            }
+            else
+            {
+                if (ageInput <= 0) MessageBox.Show("Age must be positive", "Wrong age input");
+                else age = ageInput;
+            }
+
+            if (rbtnF.IsChecked == true) gender = rbtnF.Content;
+            if (rbtnM.IsChecked == true) gender = rbtnM.Content;
+            if (rbtnOther.IsChecked == true) gender = rbtnOther.Content;
+
+            if ((bool)ckbxTiger.IsChecked) pets += ckbxTiger.Content;
+            if ((bool)ckbxLion.IsChecked) pets += ckbxLion.Content;
+            if ((bool)ckbxBear.IsChecked) pets += ckbxBear.Content;
+
+            String continent = cbbxContinent.Text;
+
+            String data = String.Format(@"{0};{1};{2};{3};{4}", name, age, gender, pets, continent) + Environment.NewLine;
+            File.AppendAllText(@"..\..\people.txt", data);
+        }
     }
 }
