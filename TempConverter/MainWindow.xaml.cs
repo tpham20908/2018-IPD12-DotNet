fb0621811eb9f8020a1f0920d20afb73028eca74
Tung Pham <tungint@gmail.com>
2018-03-15 11:34:33 -0400

    WPF practice

---
+++ TempConverter/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TempConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            double cel;
+            if (double.TryParse(tbCelcius.Text, out cel))
+            {
+                double fah = cel * 9 / 5 - 32;
+                lblFah.Content = string.Format("{0:0.00} F", fah);
+            }
+        }
+    }
+}
