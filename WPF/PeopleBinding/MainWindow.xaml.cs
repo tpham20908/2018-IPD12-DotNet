f09f26aba1e5bb565ab590e56e37b174941ad2f2
unknown <tpham20908@gmail.com>
2018-03-17 21:10:08 -0400

    WPF: PeopleBinding with class Person implement INotifyPropertyChanged

--- WPF/PeopleBinding/MainWindow.xaml.cs
+++ WPF/PeopleBinding/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -53,19 +54,46 @@ namespace PeopleBinding
             {
                 MessageBox.Show(ex.Message);
             }
+            resetFields();
         }
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
+            String idStr = lblId.Content + "";
+            String name = tbName.Text;
+            String ageStr = tbAge.Text;
+            if (int.TryParse(idStr, out int id) && int.TryParse(ageStr, out int age))
+            {
+                Person p = new Person() { ID = id, Name = name, Age = age };
+                people.Add(p);
+                lvPeople.Items.Add(p);
+            }
+            else
+            {
+                MessageBox.Show("Age must be an integer", "Wrong type input", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            resetFields();
+        }
+
+        private void lvPeople_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
             String name = ((Person)lvPeople.SelectedItem).Name;
             int age = ((Person)lvPeople.SelectedItem).Age;
             int id = ((Person)lvPeople.SelectedItem).ID;
-            MessageBox.Show(id + name + age);
+            lblId.Content = id;
+            tbName.Text = name;
+            tbAge.Text = age + "";
         }
 
+        public void resetFields()
+        {
+            lblId.Content = "...";
+            tbName.Text = "";
+            tbAge.Text = "";
+        }
     }
 
-    class Person
+    class Person : INotifyPropertyChanged
     {
         int _age;
         String _name;
@@ -80,13 +108,14 @@ namespace PeopleBinding
             }
             set
             {
-                if (value < 0 || value > 150)
+                if (value < 0 || value > 150 || _age != value)
                 {
                     throw new InvalidDataException("Age must be in between 0 and 150");
                 }
                 else
                 {
                     _age = value;
+                    OnPropertyChanged("Age");
                 }
             }
         }
@@ -99,14 +128,25 @@ namespace PeopleBinding
             }
             set
             {
-                if (value.Length < 2 || value.Length > 50)
+                if (value.Length < 2 || value.Length > 50 || _name != value)
                 {
                     throw new InvalidDataException("Name must be 2 to 50 characters length");
                 }
                 else
                 {
                     _name = value;
+                    OnPropertyChanged("Name");
                 }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
     }
