2d30ed703d835c5e4c0084cee513828a306fa12c
unknown <tpham20908@gmail.com>
2018-03-19 14:57:32 -0400

    WPF: PeopleBinding_TeacherVersion

---
+++ WPF/PeopleBinding_TeacherVersion/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PeopleBinding_TeacherVersion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Person> peopleList = new List<Person>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lvPeople.ItemsSource = peopleList;
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string name = tbName.Text;
+            string ageStr = tbAge.Text;
+            int age;
+            if (!int.TryParse(ageStr, out age))
+            {
+                MessageBox.Show("Age must be an integer");
+                return;
+            }
+            try
+            {
+                Person p = new Person(name, age);
+                peopleList.Add(p);
+                lvPeople.Items.Refresh();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MenuItemDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int index = lvPeople.SelectedIndex;
+            if (index < 0)
+            {
+                return;
+            }
+            peopleList.RemoveAt(index);
+            lvPeople.Items.Refresh();
+        }
+
+        private void lvPeople_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = lvPeople.SelectedIndex;
+            if (index < 0)
+            {
+                lblId.Content = "...";
+                return;
+            }
+            Person p = peopleList[index];
+            lblId.Content = p.Id + "";
+            tbName.Text = p.Name;
+            tbAge.Text = p.Age + "";
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            int index = lvPeople.SelectedIndex;
+            if (index < 0)
+            {
+                return;
+            }
+            string name = tbName.Text;
+            string ageStr = tbAge.Text;
+            int age;
+            if (!int.TryParse(ageStr, out age))
+            {
+                MessageBox.Show("Age must be an integer");
+                return;
+            }
+            try
+            {
+                Person p = peopleList[index];
+                p.Age = age;
+                p.Name = name;
+                lvPeople.Items.Refresh();
+            }
+            catch (ArgumentException ex)
+            {
+                // a terrible fix for a partial update problem
+                lvPeople.Items.Refresh();
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ WPF/PeopleBinding_TeacherVersion/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleBinding_TeacherVersion
+{
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            _id = ++instanceCount;
+        }
+
+        private static int instanceCount;
+
+        private int _id;
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length < 2 || value.Length > 50)
+                {
+                    throw new ArgumentException("Name must be 2-50 characters long");
+                }
+                _name = value;
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 1 || value > 150)
+                {
+                    throw new ArgumentException("Age must be 1-150");
+                }
+                _age = value;
+            }
+        }
+    }
+}
