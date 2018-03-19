da37e78346f893704ea288ea6727cd57b419f000
unknown <tpham20908@gmail.com>
2018-03-19 17:28:53 -0400

    WPF: FriendDB - designed in xaml

--- WPF/FirstDB/Program.cs
+++ WPF/FirstDB/Program.cs
@@ -11,29 +11,33 @@ namespace FirstDB
     {
         static void Main(string[] args)
         {
+            /*
             SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)
             \MSSQLLocalDB;AttachDbFilename=D:\TP\11-DotNet\2018-IPD12-DotNet\WPF
             \FirstDB\FirstDB.mdf;Integrated Security=True;Connect Timeout=30");
+            */
 
+            SqlConnection conn = new SqlConnection(@"Server = den1.mssql3.gear.host;
+            Database = jac; User Id = jac; Password = jac%5^9&17");
             conn.Open();
+            /*
             // insert a records
             SqlCommand insertCommand = new SqlCommand("INSERT INTO Friends (Name) VALUES (@Name)", conn);
-            insertCommand.Parameters.AddWithValue("@Name", "Timmy");
+            insertCommand.Parameters.AddWithValue("@Name", "Leo");
             insertCommand.ExecuteNonQuery();
-
-            /*
-            SqlCommand command = new SqlCommand("SELECT * FROM TableName WHERE FirstColumn = @0", conn);
+            */
+            SqlCommand command = new SqlCommand("SELECT * FROM friends", conn);
             using (SqlDataReader reader = command.ExecuteReader())
             {
-                Console.WriteLine("FirstColumn\tSecond Column\t\tThird Column\t\tForth Column\t");
+                Console.WriteLine("Id\tName");
                 while (reader.Read())
                 {
-                    Console.WriteLine(String.Format("{0} \t | {1}", reader[0], reader[1]));
+                    Console.WriteLine(String.Format("{0}\t{1}", reader[0], reader[1]));
                 }
             }
             
             Console.ReadLine();
-            */
+            conn.Close();
         }
     }
 }
