c868683b6b4347c1aa2a557600f73ba878f5c61f
unknown <tpham20908@gmail.com>
2018-03-20 15:58:00 -0400

    WPF: CustomSandwich v1

---
+++ WPF/CustomSanwich/CustomDialog.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomSanwich
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        public String Bread
+        {
+            get
+            {
+                return ((ComboBoxItem)cbbxBread.SelectedItem).Content.ToString();
+            }
+        }
+
+        public String Vegies
+        {
+            get
+            {
+                List<String> vegies = new List<string>();
+                if (ckbxCucumbers.IsChecked == true) vegies.Add("Cucumbers");
+                if (ckbxLettuce.IsChecked == true) vegies.Add("Lettuce");
+                if (ckbxTomatoes.IsChecked == true) vegies.Add("Tomatoes");
+                return String.Join(", ", vegies);
+            }
+        }
+
+        public String Meat
+        {
+            get
+            {
+                if (rbtnBeef.IsChecked == true) return rbtnBeef.Content + "";
+                else if (rbtnChicken.IsChecked == true)  return rbtnChicken.Content + "";
+                else return rbtnTurkey.Content + "";
+            }
+        }
+    }
+}
---
+++ WPF/CustomSanwich/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomSanwich
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMake_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 Dialog = new Window1();
+            if (Dialog.ShowDialog() == true)
+            {
+                lblBread.Content = Dialog.Bread;
+                lblMeat.Content = Dialog.Meat;
+                lblVegies.Content = Dialog.Vegies;
+            }
+        }
+    }
+}
