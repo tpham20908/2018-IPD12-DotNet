2916187c3c11e576a0d8946ed1433d84ae8319ef
unknown <tpham20908@gmail.com>
2018-03-19 20:29:45 -0400

    WPF: PeopleDB: Person class, Database class with insert and select

---
+++ WPF/PeopleDB/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleDB
+{
+    class Database
+    {
+        SqlConnection conn = new SqlConnection(@"Server = den1.mssql3.gear.host;
+            Database = jac; User Id = jac; Password = tp%ipd12");
+
+        public void insertToPeople(String name, int age, double height)
+        {
+            conn.Open();
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO People (Name, Age, Height) " +
+                "VALUES (@name, @age, @height)", conn);
+            insertCommand.Parameters.AddWithValue("@name", name);
+            insertCommand.Parameters.AddWithValue("@age", age);
+            insertCommand.Parameters.AddWithValue("@height", height);
+            insertCommand.ExecuteNonQuery();
+        }      
+
+        public List<Person> selectPeople()
+        {
+            List<Person> list = new List<Person>();
+            conn.Open();
+            SqlCommand selectCommand = new SqlCommand("SELECT * FROM People", conn);
+            using (SqlDataReader reader = selectCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int id = int.Parse(reader[0] + "");
+                    String name = reader[1] + "";
+                    int age = int.Parse(reader[2] + "");
+                    double height = double.Parse(reader[3] + "");
+                    Person p = new Person(id, name, age, height);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ WPF/PeopleDB/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleDB
+{
+    class Person
+    {
+        public Person(int id, string name, int age, double height)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Height = height;
+        }
+
+        //private static int instanceCount;
+
+        public int Id { get; set; }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length < 2 || value.Length > 50)
+                {
+                    throw new ArgumentException("Name must be 2-50 characters long");
+                }
+                _name = value;
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 1 || value > 150)
+                {
+                    throw new ArgumentException("Age must be 1-150");
+                }
+                _age = value;
+            }
+        }
+
+        private double _height;
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value < 30 || value > 300)
+                {
+                    throw new ArgumentException("Height must be 3-300");
+                }
+                _height = value;
+            }
+        }
+    }
+}
