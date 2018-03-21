0e9c2d0ac48486aaeec54303e0c2b66927ffbb70
unknown <tpham20908@gmail.com>
2018-03-21 11:34:14 -0400

    WPF: Practices/TailorModeTours starting

--- Practices/TreeViews/MainWindow.xaml.cs
+++ Practices/TreeViews/MainWindow.xaml.cs
@@ -31,10 +31,42 @@ namespace TreeViews
             // get entry logical drive on the machine
             foreach (var drive in Directory.GetLogicalDrives())
             {
+                // create new item for it
                 var item = new TreeViewItem();
+                // set the header and path
                 item.Header = drive;
+                // full path
                 item.Tag = drive;
+                // add a dummy item in item
+                item.Items.Add(null);
+                // listen out for item expanded
+                item.Expanded += Folder_Expanded;
+                // add it to the main menu
                 FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+            // if the item only contains the dummy data
+            if (item.Items.Count != 1 || item.Items[0] != null) return;
+            // clear dummy data
+            item.Items.Clear();
+            // get full path
+            var fullPath = (string)item.Tag;
+            // create a blank list for directories
+            var directories = new List<string>();
+            // try and get directories from the folder
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+            }
+            catch { }
+            directories.ForEach(directories.Path => {
+
             }
         }
     }
